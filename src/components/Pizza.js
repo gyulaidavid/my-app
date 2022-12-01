a940cfa09bd177fff302b259669e2a741ead677e
gyulaidavid <gyulaidavid@gmail.com>
2022-12-01 14:32:13 +0100

    orai munka

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Pizza.css'
 
 function Pizza({ name, id, ingredients, price, picture }) {
@@ -6,15 +6,23 @@ function Pizza({ name, id, ingredients, price, picture }) {
   //const {name} = pizzaData
 
     const [isFavorite, setIsFavorite] = useState(false)
-  
+
+    useEffect(function () {
+      console.log(isFavorite , "helló");
+    }, 
+     // [isFavorite]
+    )
+
     return (
     <div className={"Pizza" + (isFavorite ? ' fav' : '')}> 
       <img onClick={() => 
-        setIsFavorite(true)
-      }
-      src="https://as1.ftcdn.net/v2/jpg/01/09/84/42/1000_F_109844239_A7MdQSDf4y1H80cfvHZuSa0zKBkZ68S7.jpg" alt="csillag" className="favorite"/>
+       setIsFavorite(!isFavorite)}
+      
+      src={isFavorite ? "https://as1.ftcdn.net/v2/jpg/01/09/84/42/1000_F_109844239_A7MdQSDf4y1H80cfvHZuSa0zKBkZ68S7.jpg" : "https://www.iconpacks.net/icons/2/free-favourite-icon-2765-thumb.png"} alt="csillag" className="favorite"/>
+
       <p>Sorszám: {id}</p>
-      <h1>{name} {isFavorite ? 'Kedvenc' : 'Fújjjjj'}</h1>
+      <div><img src={"pictures/" + picture} alt="pizza pictures"/></div>
+      <h1>{name} {isFavorite ? 'Kedvenc' : 'Fujjjjj'}</h1>
       <h3>Összetevők: {ingredients}</h3>
       <h2>{price} Ft</h2>
       <hr />
@@ -23,3 +31,4 @@ function Pizza({ name, id, ingredients, price, picture }) {
 }
 
 export default Pizza;
+//"https://www.iconpacks.net/icons/2/free-favourite-icon-2765-thumb.png"
--- src/components/Pizzas.js
+++ src/components/Pizzas.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import {pizzas} from '../data/pizza_data'
-import Pizza from './Pizza'
+import React, { useEffect, useState } from "react";
+import Pizza from "./Pizza";
 
 function Pizzas() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:3005")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setPizzas(data);
+      });
+  }, []);
+
   return (
     <div>
-     {pizzas.map(pizza => <Pizza name={pizza.name} key={pizza.id} id={pizza.id} price={pizza.price} ingredients={pizza.ingredients}/>) }   
-
+      {pizzas.map((pizza) => (
+        <Pizza
+          name={pizza.name}
+          key={pizza.id}
+          id={pizza.id}
+          price={pizza.price}
+          ingredients={pizza.ingredients}
+          picture={pizza.picture}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Pizzas
+export default Pizzas;
